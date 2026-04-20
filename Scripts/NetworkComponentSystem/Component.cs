1a05935fc4ca45fabcc5b5d6f0a99cd650421120
Hybrid46 <hybrid_46_2@hotmail.com>
2026-04-20 21:23:35 +0200

    Component to bit conversion

--- Scripts/NetworkComponentSystem/Component.cs
+++ Scripts/NetworkComponentSystem/Component.cs
@@ -15,6 +15,18 @@
 
         public Entity Entity { get; set; }
         public bool Dirty { get; set; } = true;
+
+        public static ComponentBits ToBits<T>() where T : Component
+        {
+            if (typeof(T) == typeof(Transform)) return ComponentBits.Transform;
+            if (typeof(T) == typeof(HealthComponent)) return ComponentBits.Health;
+            if (typeof(T) == typeof(MovementComponent)) return ComponentBits.Movement;
+            if (typeof(T) == typeof(BulletHealthComponent)) return ComponentBits.BulletHealth;
+            if (typeof(T) == typeof(DrawComponent)) return ComponentBits.Draw;
+            if (typeof(T) == typeof(ControllerComponent)) return ComponentBits.Controller;
+            return ComponentBits.Transform;
+        }
+
         public void MarkDirty() => Dirty = true;
         public void ResetDirty() => Dirty = false;
     }
